6e0a3d01844a165300684d9be9474feeeecf85ba
lian wong <ltaylor.wong@gmail.com>
2015-10-05 04:49:14 -0400

    len&copy

--- stringy.c
+++ stringy.c
@@ -16,5 +16,19 @@ int my_strlen(char *s){
 //strcpy: (char *dest, char *source) copies all the characters up to 
 //and including the null returns dest
 char my_strcpy(char *destination, char *source){
+}
 
+//strcmp: returns 0 if s1 and s2 are identical
+//postitive if more, negative if less
+int my_strcmp(char *s1, char *s2){
+}
+
+//strncat:
+my_strncat(){
+}
+//strchr:
+my_strchr(){
+}
+//strstr:
+my_strstr(){
 }
