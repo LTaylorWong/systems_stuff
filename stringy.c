a8b0ead8263cf359c7cb584fa8fa0f0f9d668b61
lian wong <ltaylor.wong@gmail.com>
2015-10-05 04:40:19 -0400

    len&copy

--- stringy.c
+++ stringy.c
@@ -1,1 +1,20 @@
-#include<>
+#include <stdio.h>
+#include <stdlib.h>
+
+//strlen:returns the number of characters form the sttar of s up to
+//but not including the null
+int my_strlen(char *s){
+  int x = 0;
+  int len = 0;
+  while(s1[x] != 0){
+    len++;
+    x++;
+  }
+  return len;
+}
+
+//strcpy: (char *dest, char *source) copies all the characters up to 
+//and including the null returns dest
+char my_strcpy(char *destination, char *source){
+
+}
